ee07201f9917da3e0dbb58b06fdac6bad2d02805
UntestedEngineer <cpiehler2@gmail.com>
2023-12-31 14:00:02 -0500

    Initial release

---
+++ Original Files/README.md
@@ -0,0 +1,45 @@
+# ddclient docker
+
+This application is forked from [https://github.com/steasdal/ddclient-alpine](https://github.com/steasdal/ddclient-alpine) with heavy modifications.  Namely most of the environment variables have been removed and is now reliant on injecting a configuration during container creation.  The original files have been preserved for reference under the "Original Files" folder.  
+
+Original ddclient documentation can be located: [ddclient homepage](https://ddclient.net/)
+
+## Usage
+
+Since I run mostly Kubernetes all yaml manifests are designed towards the solution.  However, docker-compose can be adapted as necessary.
+
+Create a config file for the relevant ddns provider of choice.  Examples are located here: [ddclient examples](https://github.com/ddclient/ddclient/blob/master/ddclient.conf.in)
+
+_Note if running in a container "use=web" must be set since the application can not automatically derive the IP address from the internal container network_
+
+**Keep this config file in a secure location that is not easily accessible**
+
+The following environment are relevant:
+```
+- DDNS_DAEMON_OR_ONESHOT - specifies whether to run the application as a daemon or once (oneshot)
+- DDNS_DAEMON_REFRESH_INTERVAL - specifies how frequenty the application will run if DDNS_DAEMON_OR_ONESHOT is set to daemon (defaults to 30 seconds if not specified)
+```
+
+### Kubernetes
+
+Create a secret based on the above config file since it has sensitive information.
+
+```
+kubectl create secret generic -n some-namespace some-secret --from-file=ddclient.conf
+```
+
+Use this secret as a volume mount and ensure read-only minimal permissions are provided.  An example deployment manifest is provided in the Kubernetes directory
+
+### Docker
+
+Docker unfortunately does not have a good way of securing secrets unless using docker swarm.  Therefore store the config file on the docker host and expose it via a bind mount that is ro (read only).  Ensure the host directory/file are secured with tight permissions.
+
+```
+services:
+  <service_name>:
+    image: <image_name>
+    volumes:
+      - <host_directory>:/etc/ddclient/:ro
+```
+
+
---
+++ Original Files/entrypoint-original.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+set -e
+CONF_FILE="/etc/ddclient.conf"
+cp ${CONF_FILE}.original ${CONF_FILE}
+mkdir -p /etc/ddclient/
+mkdir -p /var/cache/ddclient/
+ln -s /etc/ddclient.conf /etc/ddclient/ddclient.conf
+
+: "${DDNS_DAEMON_OR_ONESHOT?Need to set DDNS_DAEMON_OR_ONESHOT env var}"
+: "${DDNS_SERVER?Need to set DDNS_SERVER env var}"
+: "${DDNS_PROTOCOL?Need to set DDNS_PROTOCOL env var}"
+: "${DDNS_USERNAME?Need to set DDNS_USERNAME env var}"
+: "${DDNS_PASSWORD?Need to set DDNS_PASSWORD env var}"
+: "${DDNS_DOMAIN?Need to set DDNS_DOMAIN env var}"
+: "${DDNS_AUTOPUBLIC_OR_INTERFACE?Need to set DDNS_AUTOPUBLIC_OR_INTERFACE env var, chose autopublic, or a proper interface name such as eth0}"
+
+# Set the web checkip url to the value of the DDNS_CHECKIP_URL environment
+# variable if it's present; default to checkip.dyndns.com if it's not.
+CHECKIP_URL=${DDNS_CHECKIP_URL:-checkip.dyndns.com}
+
+if  [ "$DDNS_AUTOPUBLIC_OR_INTERFACE" = "autopublic" ]
+then
+    echo "use=web, web=$CHECKIP_URL/, web-skip='IP Address'" >> "${CONF_FILE}"
+else
+    INTERFACE_LIST=`cat /proc/net/dev | grep ":" | awk -F ":" '{print $1}' | awk '{$1=$1;print $1}'`
+    result=`echo "${INTERFACE_LIST}" | grep "^${DDNS_AUTOPUBLIC_OR_INTERFACE}$" | wc -l`
+    if ! [ "${result}" = "1" ]
+    then
+        echo "DDNS_AUTOPUBLIC_OR_INTERFACE should be set to autopublic or to one of the available interfaces"
+        echo "$INTERFACE_LIST"
+        exit
+    fi
+    echo "use=if, if=${DDNS_AUTOPUBLIC_OR_INTERFACE}" >>  "${CONF_FILE}"
+fi
+sed -i "s/SERVER/${DDNS_SERVER}/g" ${CONF_FILE}
+sed -i "s/PROTOCOL/${DDNS_PROTOCOL}/g" ${CONF_FILE}
+sed -i "s/USERNAME/${DDNS_USERNAME}/g" ${CONF_FILE}
+sed -i "s/PASSWORD/${DDNS_PASSWORD}/g" ${CONF_FILE}
+echo "${DDNS_DOMAIN}" >>  "${CONF_FILE}"
+
+# Set the daemon refresh interval to the value of the DDNS_DAEMON_REFRESH_INTERVAL
+# environment variable if it's present; default to 30 seconds if it's not.
+DAEMON_REFRESH_INTERVAL=${DDNS_DAEMON_REFRESH_INTERVAL:-30}
+
+if  [ "$DDNS_DAEMON_OR_ONESHOT" = "daemon" ]
+then
+    echo "daemon=$DAEMON_REFRESH_INTERVAL" >> ${CONF_FILE}
+    ddclient -foreground -verbose -daemon=$DAEMON_REFRESH_INTERVAL
+elif [ "$DDNS_DAEMON_OR_ONESHOT" = "oneshot" ]
+then
+    ddclient -foreground -verbose -daemon=0
+else
+    echo "DDNS_DAEMON_OR_ONESHOT should be set to daemon or to oneshot"
+fi
